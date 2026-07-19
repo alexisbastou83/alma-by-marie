b42e4de49afce05b3bea8ac6e9f62dcaaa40d1c0
alexisbastou83 <alexisbastou@gmail.com>
2026-07-19 19:30:53 +0200

    Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/",
-  base: "/alma-by-marie/",
+  base: "/",
+  // base: "/alma-by-marie/",
   plugins: [react()],
   css: {
     lightningcss: {
